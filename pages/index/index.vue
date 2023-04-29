4db9618577dbc4267021e430524b1b6c13a72943
lasertsole <56973455+lasertsole@users.noreply.github.com>
2023-04-29 11:00:16 +0800

    加入了uni-ui

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -12,6 +12,9 @@
 		
 		<template #default>
 			<searchBar></searchBar>
+			<uni-card title="基础卡片" sub-title="副标题" extra="额外信息" :thumbnail="avatar" @click="onClick">
+				<text class="uni-body">这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+			</uni-card>
 		</template>
 	</bodyer>
 </template>
